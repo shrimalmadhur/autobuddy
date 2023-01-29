70382dcb2fc76736e7292d0d6ddabf84e8c6c856
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-01-28 16:43:18 -0800

    add webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -7,7 +7,7 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   // res.status(200).json({ name: 'John Doe' })
   /**
@@ -27,7 +27,7 @@ export default function handler(
      if (mode === "subscribe" && token === verify_token) {
        // Respond with 200 OK and challenge token from the request
        console.log("WEBHOOK_VERIFIED");
-       res.status(200).send({name: challenge});
+       res.status(200).send(challenge);
      } else {
        // Responds with '403 Forbidden' if verify tokens do not match
        res.status(403);
