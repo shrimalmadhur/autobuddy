342d260c7a06f3cb6531a5c73101cdf842aeb299
Madhur Shrimal <madhur.shrimal@gmail.com>
2023-01-28 16:41:05 -0800

    add webhook

---
+++ pages/api/webhook.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string | string[] | undefined
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // res.status(200).json({ name: 'John Doe' })
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+  **/
+   const verify_token = process.env.NEXT_VERIFY_TOKEN;
+
+   // Parse params from the webhook verification request
+   let mode = req.query["hub.mode"];
+   let token = req.query["hub.verify_token"];
+   let challenge = req.query["hub.challenge"];
+ 
+   // Check if a token and mode were sent
+   if (mode && token) {
+     // Check the mode and token sent are correct
+     if (mode === "subscribe" && token === verify_token) {
+       // Respond with 200 OK and challenge token from the request
+       console.log("WEBHOOK_VERIFIED");
+       res.status(200).send({name: challenge});
+     } else {
+       // Responds with '403 Forbidden' if verify tokens do not match
+       res.status(403);
+     }
+   }
+}
